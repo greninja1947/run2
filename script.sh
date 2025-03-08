e22e2e5be5f02fd8a4b0e523ef7803bf9b6b90c6
Anoop Singh <cs1230459@iitd.ac.in>
2025-03-08 17:20:39 +0530

    hope you will enjoy the hunt: commit 1

---
+++ script.sh
@@ -0,0 +1,8 @@
+for i in $(seq 1 100); do
+  # Append a unique line to readme.md
+  echo "This is commit number $i" > READEME.md
+  
+  # Stage and commit the change
+  git add .
+  git commit -m "hope you will enjoy the hunt: commit $i"
+done
